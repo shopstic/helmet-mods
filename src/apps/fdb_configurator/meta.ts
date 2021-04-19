dfcdced81bce28d586a2ab14ea9f69e1157158e5
CI Runner <ci-runner@shopstic.com>
2021-04-19 09:09:32 +0000

    Release 1.1.5

--- src/apps/fdb_configurator/meta.ts
+++ src/apps/fdb_configurator/meta.ts
@@ -1,3 +1,3 @@
-export const version = "1.1.4";
+export const version = "1.1.5";
 export const imageName = "k8s-fdb-configurator";
 export const autoVersioning = true;
--- src/apps/iac_version_bumper/meta.ts
+++ src/apps/iac_version_bumper/meta.ts
@@ -1,3 +1,3 @@
-export const version = "1.1.4";
+export const version = "1.1.5";
 export const imageName = "iac-version-bumper";
 export const autoVersioning = true;
