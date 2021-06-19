1a8f128305b7cc0ff507992bc2eacf593fdaf0b5
CI Runner <ci-runner@shopstic.com>
2021-06-19 06:36:25 +0000

    Release 1.8.4

--- src/apps/fdb_configurator/meta.ts
+++ src/apps/fdb_configurator/meta.ts
@@ -1,2 +1,2 @@
-export const version = "b0ab3e90a9489eab12d326f3a10de06cfb8831f6";
+export const version = "4d832590ce8abf6bfbcbabeb704ede5eebc038b9";
 export const imageName = "k8s-fdb-configurator";
