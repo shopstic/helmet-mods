c42c928d158695a4206679a623a1f781c6febf90
Jacky Nguyen <nktpro@gmail.com>
2021-06-27 19:37:57 -0400

    Update helmet to 1.4.14

--- src/deps/async_utils.ts
+++ src/deps/async_utils.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/helmet@1.4.12/deps/async_utils.ts";
+export * from "https://deno.land/x/helmet@1.4.14/deps/async_utils.ts";
--- src/deps/std_fmt_colors.ts
+++ src/deps/std_fmt_colors.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/helmet@1.4.12/deps/std_fmt_colors.ts";
+export * from "https://deno.land/x/helmet@1.4.14/deps/std_fmt_colors.ts";
