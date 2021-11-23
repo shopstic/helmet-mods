d3352293057e22d974aad4a2a96acc5c923c0052
CI Runner <ci-runner@shopstic.com>
2021-11-23 03:20:48 -0500

    Fix release script

--- release.sh
+++ release.sh
@@ -44,17 +44,17 @@ release() {
   local FDB_CONFIGURATOR_MANIFEST
   local IAC_VERSION_BUMPER_MANIFEST
 
-  FDB_SERVER_MANIFEST=$(manifest-tool inspect --raw docker.io/shopstic/fdb-server:"${RELEASE_TAG}" | jq -r '.digest')
-  FDB_CONFIGURATOR_MANIFEST=$(manifest-tool inspect --raw docker.io/shopstic/fdb-configurator:"${RELEASE_TAG}" | jq -r '.digest')
-  IAC_VERSION_BUMPER_MANIFEST=$(manifest-tool inspect --raw docker.io/shopstic/iac-version-bumper:"${RELEASE_TAG}" | jq -r '.digest')
+  FDB_SERVER_MANIFEST=$(manifest-tool inspect --raw docker.io/shopstic/fdb-server:"${IMAGE_TAG}" | jq -r '.digest')
+  FDB_CONFIGURATOR_MANIFEST=$(manifest-tool inspect --raw docker.io/shopstic/fdb-configurator:"${IMAGE_TAG}" | jq -r '.digest')
+  IAC_VERSION_BUMPER_MANIFEST=$(manifest-tool inspect --raw docker.io/shopstic/iac-version-bumper:"${IMAGE_TAG}" | jq -r '.digest')
 
   git config --global user.email "ci-runner@shopstic.com"
   git config --global user.name "CI Runner"
   git checkout -b releases/"${RELEASE_VERSION}"
 
   patch_app_meta ./src/apps/fdb/meta.ts fdb-server "${FDB_SERVER_MANIFEST}"
-  patch_app_meta ./src/apps/fdb-configurator/meta.ts fdb-configurator "${FDB_CONFIGURATOR_MANIFEST}"
-  patch_app_meta ./src/apps/iac-version-bumper/meta.ts iac-version-bumper "${IAC_VERSION_BUMPER_MANIFEST}"
+  patch_app_meta ./src/apps/fdb_configurator/meta.ts fdb-configurator "${FDB_CONFIGURATOR_MANIFEST}"
+  patch_app_meta ./src/apps/iac_version_bumper/meta.ts iac-version-bumper "${IAC_VERSION_BUMPER_MANIFEST}"
 
   echo "export default \"${RELEASE_VERSION}\";" > ./src/version.ts
 
@@ -64,14 +64,11 @@ release() {
 }
 
 patch_app_meta() {
-  local PATH=${1:?"Path is required"}
+  local META_PATH=${1:?"Path is required"}
   local IMAGE_NAME=${2:?"Image name is required"}
   local VERSION=${3:?"Version is required"}
 
-printenv PATH
-which cat
-
-cat <<EOF > "${PATH}"
+cat <<EOF > "${META_PATH}"
 export const version = "${VERSION}";
 export const imageName = "${IMAGE_NAME}";
 
