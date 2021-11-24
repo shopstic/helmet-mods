101cdb9736e155d1b20dc5153a4533eb3abfb576
CI Runner <ci-runner@shopstic.com>
2021-11-24 09:46:26 +0000

    Release 1.11.9

--- src/apps/iac_version_bumper/meta.ts
+++ src/apps/iac_version_bumper/meta.ts
@@ -1,1 +1,1 @@
-export const image = "docker.io/shopstic/iac-version-bumper:latest";
+export const image = "docker.io/shopstic/iac-version-bumper@sha256:98707b807b63a9f82509e8a7bc5b87190b5afa0f9c6ae091c5319d53f0639e2d";
