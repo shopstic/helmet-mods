59fe780f55b6652337f97e307e422601505ec2d9
CI Runner <ci-runner@shopstic.com>
2022-01-22 20:50:26 +0000

    Release 1.13.3

--- src/apps/fdb/meta.ts
+++ src/apps/fdb/meta.ts
@@ -1,1 +1,1 @@
-export const image = "docker.io/shopstic/fdb-server:latest";
+export const image = "public.ecr.aws/shopstic/fdb-server@sha256:4000270e5173e701aa803c85810cfe01e7b7f4066923e6488ea6891c4bdfd61a";
--- src/apps/iac_version_bumper/meta.ts
+++ src/apps/iac_version_bumper/meta.ts
@@ -1,1 +1,1 @@
-export const image = "docker.io/shopstic/iac-version-bumper:latest";
+export const image = "public.ecr.aws/shopstic/iac-version-bumper@sha256:aee14bccc1b4a83d2eab395ccdfe3f1e07065de6ac2fd17be6d381e4ef78b937";
