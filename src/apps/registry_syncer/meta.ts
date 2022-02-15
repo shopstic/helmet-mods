6ed47992a7c3c06c387f9b398c356fdc68c7bf26
CI Runner <ci-runner@shopstic.com>
2022-02-15 06:25:52 +0000

    Release 1.16.7

--- src/apps/registry_syncer/meta.ts
+++ src/apps/registry_syncer/meta.ts
@@ -1,1 +1,1 @@
-export const image = "public.ecr.aws/shopstic/registry-syncer:latest";
+export const image = "public.ecr.aws/shopstic/registry-syncer@sha256:b1d9fd3d7f7726c7564e7e144b6696535d2061d84cf5bc5ab2106645b6b6438b";
