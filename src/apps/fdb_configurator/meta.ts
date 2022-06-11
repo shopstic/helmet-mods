03b0a33ee2c15260528e974098c7e7e41b72d812
CI Runner <ci-runner@shopstic.com>
2022-06-11 20:52:49 +0000

    Release 1.19.0

--- src/apps/fdb_configurator/meta.ts
+++ src/apps/fdb_configurator/meta.ts
@@ -1,1 +1,1 @@
-export const image = "public.ecr.aws/shopstic/fdb-configurator:latest";
+export const image = "public.ecr.aws/shopstic/fdb-configurator@sha256:b41c1f077e4930ee7605047fca88c18f6ca644e4550838512b8af6c56c97db12";
