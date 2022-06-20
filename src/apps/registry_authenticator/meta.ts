ea4ecc8eb219e67699793d34b4d699d934655a7a
CI Runner <ci-runner@shopstic.com>
2022-06-20 04:16:34 +0000

    Release 1.19.11

--- src/apps/registry_authenticator/meta.ts
+++ src/apps/registry_authenticator/meta.ts
@@ -1,1 +1,1 @@
-export const image = "public.ecr.aws/shopstic/registry-authenticator:latest";
+export const image = "public.ecr.aws/shopstic/registry-authenticator@sha256:886a59d3ec17ad501b590ac65451f35d15894c01c61cb64a7fa7800430cf3412";
--- src/apps/registry_syncer/meta.ts
+++ src/apps/registry_syncer/meta.ts
@@ -1,1 +1,1 @@
-export const image = "public.ecr.aws/shopstic/registry-syncer:latest";
+export const image = "public.ecr.aws/shopstic/registry-syncer@sha256:6f3fcf03adf81d82648f85728e7559b33523a2f9ee06df683233c309aa827a10";
