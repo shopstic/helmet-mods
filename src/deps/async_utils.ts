e359265af58d6ede18fbed879dfe14a389a3fd9c
Jacky Nguyen <nktpro@gmail.com>
2022-07-28 23:25:33 -0400

    Update helmet dependency

--- src/deps/async_utils.ts
+++ src/deps/async_utils.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/helmet@1.13.0/deps/async_utils.ts";
+export * from "https://deno.land/x/helmet@1.14.0/deps/async_utils.ts";
--- src/deps/typebox.ts
+++ src/deps/typebox.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/helmet@1.13.0/deps/typebox.ts";
+export * from "https://deno.land/x/helmet@1.14.0/deps/typebox.ts";
