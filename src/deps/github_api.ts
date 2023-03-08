368721c0f6824dc245c8b913b286e25b80ce8da9
Jacky Nguyen <nktpro@gmail.com>
2023-03-08 18:18:06 -0500

    Update @octokit/webhooks types

--- src/deps/github_api.ts
+++ src/deps/github_api.ts
@@ -1,3 +1,2 @@
-// @deno-types="https://cdn.shopstic.com/pin/@octokit/webhooks-types@v6.3.5-RJQue4RaVn41BlDahcM2/dist=es2019,mode=types/schema.d.ts"
-export * from "https://cdn.shopstic.com/pin/@octokit/webhooks-types@v6.3.5-RJQue4RaVn41BlDahcM2/mode=imports/optimized/@octokit/webhooks-types.js";
+export * from "https://raw.githubusercontent.com/octokit/webhooks/v6.10.0/payload-types/schema.d.ts";
 export type { components as GhComponents, paths as GhPaths } from "https://deno.land/x/gh_fetch@1.0.0/index.ts";
