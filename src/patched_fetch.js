4623719b404fa3f0bbf1cbe5225c3e7708e9a601
Jacky Nguyen <nktpro@gmail.com>
2023-05-04 21:11:12 -0400

    Improve fetch patch with scope

--- src/patched_fetch.js
+++ src/patched_fetch.js
@@ -1,21 +1,23 @@
 // https://github.com/denoland/deno/issues/16487
-const oldFetch = globalThis.fetch;
-globalThis.fetch = async (input, init) => {
-  const signal = init?.signal;
-  if (signal && !signal.aborted) {
-    const abortController = new AbortController();
-    const onAbort = (event) => {
-      abortController.abort(event.target && "reason" in event.target ? event.target.reason : undefined);
-    };
-    signal.addEventListener("abort", onAbort);
-    try {
-      return await oldFetch(input, {
-        ...init,
-        signal: abortController.signal,
-      });
-    } finally {
-      signal.removeEventListener("abort", onAbort);
+globalThis.fetch = (() => {
+  const oldFetch = globalThis.fetch;
+  return async (input, init) => {
+    const signal = init?.signal;
+    if (signal && !signal.aborted) {
+      const abortController = new AbortController();
+      const onAbort = (event) => {
+        abortController.abort(event.target && "reason" in event.target ? event.target.reason : undefined);
+      };
+      signal.addEventListener("abort", onAbort);
+      try {
+        return await oldFetch(input, {
+          ...init,
+          signal: abortController.signal,
+        });
+      } finally {
+        signal.removeEventListener("abort", onAbort);
+      }
     }
-  }
-  return await oldFetch(input, init);
-};
+    return await oldFetch(input, init);
+  };
+})()
