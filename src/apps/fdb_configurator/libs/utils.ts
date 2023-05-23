857943ac82a0f7bada0ccfb91b65e51ac018a3e3
Jacky Nguyen <nktpro@gmail.com>
2023-05-22 21:40:56 -0400

    In fdb cluster, fix fetchServiceEndpointsByLabels

--- src/apps/fdb_configurator/libs/utils.ts
+++ src/apps/fdb_configurator/libs/utils.ts
@@ -271,7 +271,7 @@ export async function fetchServiceEndpointsByLabels(
     schema: ListOfServiceSpecSchema,
   });
 
-  return items.items.map((item) => `${item.spec.clusterIP}:${item.spec.ports[0]!.port}`);
+  return items.items.flatMap((item) => item.spec.ports.map((port) => `${item.spec.clusterIP}:${port}`));
 }
 
 export async function fetchPodIpsByLabels(
