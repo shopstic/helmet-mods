d87ad8eefca1119901081134baa04a24d68daa50
Jacky Nguyen <nktpro@gmail.com>
2024-03-14 23:16:03 -0400

    Update dependencies

--- src/deps/helmet.ts
+++ src/deps/helmet.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/helmet@1.27.2/mod.ts";
+export * from "https://deno.land/x/helmet@1.27.3/mod.ts";
--- src/deps/std_fs.ts
+++ src/deps/std_fs.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/helmet@1.27.2/deps/std_fs.ts";
+export * from "https://deno.land/x/helmet@1.27.3/deps/std_fs.ts";
