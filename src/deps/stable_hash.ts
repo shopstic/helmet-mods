b121727f0d0bfdfe84181c4c0fd6af366d7749e1
Jacky Nguyen <nktpro@gmail.com>
2024-04-10 17:05:03 -0400

    Fix stable-hash dep

--- src/deps/stable_hash.ts
+++ src/deps/stable_hash.ts
@@ -1,7 +1,8 @@
-import originalStableHash from "npm:stable-hash@0.0.2";
+import originalStableHash from "npm:stable-hash@0.0.4";
 import { crypto } from "jsr:@std/crypto@^0.221.0/crypto";
 import { encodeHex } from "jsr:@std/encoding@^0.221.0/hex";
 
 export function stableHash(obj: unknown) {
-  return encodeHex(crypto.subtle.digestSync("SHA-256", new TextEncoder().encode(originalStableHash(obj))));
+  // deno-lint-ignore no-explicit-any
+  return encodeHex(crypto.subtle.digestSync("SHA-256", new TextEncoder().encode((originalStableHash as any)(obj))));
 }
