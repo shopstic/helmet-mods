84c37a03e3182f7f81ef28f0cc13ef4389febbf8
CI Runner <ci-runner@shopstic.com>
2024-05-01 19:10:29 +0000

    Release 1.54.5

--- src/apps/gitlab_cicd_registry/meta.ts
+++ src/apps/gitlab_cicd_registry/meta.ts
@@ -1,1 +1,1 @@
-export const image = "public.ecr.aws/shopstic/gitlab-cicd-registry:latest";
+export const image = "public.ecr.aws/z8i7w3s6/gitlab-cicd-registry@sha256:aee7ab86cb35da348bc6d0b0ffef0ca38c4a9b33d93b0f4a38ee1c01016a348c";
--- src/apps/k8s_job_autoscaler/meta.ts
+++ src/apps/k8s_job_autoscaler/meta.ts
@@ -1,1 +1,1 @@
-export const image = "public.ecr.aws/shopstic/k8s-job-autoscaler:latest";
+export const image = "public.ecr.aws/z8i7w3s6/k8s-job-autoscaler@sha256:b8864de90b498c283da42e27e378d24a67718e76ccc1b009240ae4851284f598";
