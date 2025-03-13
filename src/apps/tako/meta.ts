a028fd0494b31488b0b473d790bd3ebb4b9a7080
CI Runner <ci-runner@shopstic.com>
2025-03-13 18:35:44 +0000

    Release 4.1.7

--- src/apps/tako/meta.ts
+++ src/apps/tako/meta.ts
@@ -1,1 +1,1 @@
-export const image = "public.ecr.aws/z8i7w3s6/tako:latest";
+export const image = "public.ecr.aws/z8i7w3s6/tako@sha256:398ae5f81028fa98830c770967259668edf6833f4ec598b27e1b31efddb97e24";
